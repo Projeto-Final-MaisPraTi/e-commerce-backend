3aa39d486332e8b866ff6a963a32cc7270c700ad
Gabriel W <gabriel.beck03@gmail.com>
2024-11-11 15:10:59 -0300

    modificado table usuarios para users no model users

--- app/src/main/java/com/ecommerce/app/model/user/User.java
+++ app/src/main/java/com/ecommerce/app/model/user/User.java
@@ -20,7 +20,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Data
-@Table(name = "usuarios")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
